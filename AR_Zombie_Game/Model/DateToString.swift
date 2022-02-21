782602f52a62ec2221bd3c46060424d36b2c8d1e
godo <godo129@naver.com>
2022-02-21 13:45:08 +0900

    모델 추가

---
+++ AR_Zombie_Game/Model/DateToString.swift
@@ -0,0 +1,16 @@
+//
+//  DateToString.swift
+//  AR_Zombie_Game
+//
+//  Created by hong on 2022/02/21.
+//
+
+import Foundation
+
+struct DateToString {
+    
+    var Year : String
+    var Month : String
+    var Date : String
+    
+}
---
+++ AR_Zombie_Game/Model/GamePointData.swift
@@ -0,0 +1,16 @@
+//
+//  GamePointData.swift
+//  AR_Zombie_Game
+//
+//  Created by hong on 2022/02/21.
+//
+
+import Foundation
+
+struct GamePoint {
+    
+    var nickName : String
+    var Date : String
+    var Point : DateToString
+    
+}
