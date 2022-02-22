2066488c903e31a48637d09f08bff2678dc5fc77
godo <godo129@naver.com>
2022-02-22 14:03:14 +0900

    건 모델

---
+++ AR_Zombie_Game/Model/GunModel.swift
@@ -0,0 +1,14 @@
+//
+//  GunModel.swift
+//  AR_Zombie_Game
+//
+//  Created by hong on 2022/02/22.
+//
+
+import Foundation
+
+struct gunModel {
+    var name : String
+    var totalBullets : Int
+    var remainBullets : Int
+}
