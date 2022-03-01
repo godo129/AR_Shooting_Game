09d0b981a34115256990629ac87aa54496af3579
godo <godo129@naver.com>
2022-03-01 14:18:56 +0900

    게임 매인 뷰에서 저장 뷰 추가

--- AR_Zombie_Game/Controller/GameMenuView.swift
+++ AR_Zombie_Game/Controller/GameMenuView.swift
@@ -10,13 +10,33 @@ import UIKit
 class GameMenuView: UIView {
 
     private let restartButton = GameMenuButton(color: UIColor.systemBlue, text: "Restart")
-    private let restorePointButton = GameMenuButton(color: UIColor.systemRed, text: "Restore Point")
+    private let savePointButton = GameMenuButton(color: UIColor.systemRed, text: "Save Point")
+    
+    let saveView = GamePointSaveView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.addSubview(restartButton)
-        self.addSubview(restorePointButton)
+        self.addSubview(savePointButton)
+        
+        
+        savePointButton.addTarget(self, action: #selector(savePointButtonTapped), for: .touchUpInside)
+        
+    }
+    
+    private func 서브뷰초기화() {
+        restartButton.removeFromSuperview()
+        savePointButton.removeFromSuperview()
+    }
+    
+    private func addSaveView() {
+        서브뷰초기화()
+        self.addSubview(saveView)
+    }
+    
+    @objc private func savePointButtonTapped() {
+        addSaveView()
     }
     
     required init?(coder: NSCoder) {
@@ -28,6 +48,8 @@ class GameMenuView: UIView {
         restartButton.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
         restartButton.center = self.center
         
-        restorePointButton.frame = CGRect(x: self.restartButton.frame.origin.x , y: self.restartButton.frame.origin.y+200, width: 200, height: 100)
+        savePointButton.frame = CGRect(x: self.restartButton.frame.origin.x , y: self.restartButton.frame.origin.y+200, width: 200, height: 100)
+        
+        saveView.frame = self.bounds
     }
 }
