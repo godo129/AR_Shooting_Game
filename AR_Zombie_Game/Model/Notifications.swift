bcfd01dd3247441ed8c4494e2426397dbad5e5fb
godo <godo129@naver.com>
2022-03-02 15:39:21 +0900

    노티피케이션

---
+++ AR_Zombie_Game/Model/Notifications.swift
@@ -0,0 +1,12 @@
+//
+//  Notifications.swift
+//  AR_Zombie_Game
+//
+//  Created by hong on 2022/03/02.
+//
+
+import Foundation
+
+let gamePointSaveViewExit = Notification.Name.init(rawValue: "gamePointSaveViewExit")
+let gameMenuViewExit = Notification.Name.init(rawValue: "gameMenuViewExit")
+
