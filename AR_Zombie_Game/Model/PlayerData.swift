5f5c22432ca1d1deef8fe7c4e3464ef697031136
godo <godo129@naver.com>
2022-03-03 16:30:02 +0900

    플레이어 인포메이션

---
+++ AR_Zombie_Game/Model/PlayerData.swift
@@ -0,0 +1,10 @@
+//
+//  PlayerData.swift
+//  AR_Zombie_Game
+//
+//  Created by hong on 2022/03/03.
+//
+
+import Foundation
+
+var player = PlayerInfomation(AmountOfMagazine: 10, curAmouontOfBullet: 10)
