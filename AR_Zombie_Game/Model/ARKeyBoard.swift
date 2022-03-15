cc10868b0bc8eb7139e5f0585d30d81c7ed7278e
godo <godo129@naver.com>
2022-03-15 19:20:21 +0900

    ARKey shift 누를 때마다 키 바꾸게... 및 키 만드는 함수

--- AR_Zombie_Game/Model/ARKeyBoard.swift
+++ AR_Zombie_Game/Model/ARKeyBoard.swift
@@ -10,6 +10,22 @@ import SceneKit
 
 class ARKeyBoard: SCNNode {
     
+    var keyboard = SCNBox(width: 0.8, height: 0.2, length: 0.1, chamferRadius: 0)
+    
+    var pressedShift = false
+    
+    let keyboardListNotPressedShift = [["₩","1","2","3","4","5","6","7","8","9","0","-","=","<-"],
+                                ["q","w","e","r","t","y","u","i","o","p","[","]","\\"],
+                                ["a","s","d","f","g","h","j","k","l",";","'","ENTER"],
+                                ["SHIFT","z","x","c","v","b","n","m",",",".","/","SHIFT"],
+                                ["SPACE"]]
+    
+    let keyboardListPressedShift = [["~","!","@","#","$","%","^","&","*","(",")","_","+","<-"],
+                                    ["Q","W","E","R","T","Y","U","I","O","P","{","}","|"],
+                                    ["A","S","D","F","G","H","J","K","L",":","\"","ENTER"],
+                                    ["SHIFT","Z","X","C","V","B","N","M","<",">","?","SHIFT"],
+                                    ["SPACE"]]
+    
     init(position: SCNVector3) {
         super.init()
         
@@ -21,6 +37,7 @@ class ARKeyBoard: SCNNode {
         let keyboard = SCNBox(width: 0.8, height: 0.2, length: 0.1, chamferRadius: 0)
         let keyboardMaterial = SCNMaterial()
         var keyboardMaterials = [SCNMaterial]()
+        // 색 변경 
         for i in 0..<6 {
             let material = keyboardMaterial
             if i == 0 {
@@ -39,15 +56,23 @@ class ARKeyBoard: SCNNode {
         
         // 키 버튼 ... 일반 버튼, shift, enter, space 는 다른 크기로 ... 대신 height 는 다 동일 , width만 다름
         
-        let keyboardListNotShift = [["`","1","2","3","4","5","6","7","8","9","0","-","=","<-"],
-                                    ["q","w","e","r","t","y","u","i","o","p","[","]","\\"],
-                                    ["a","s","d","f","g","h","j","k","l",";","'","enter"],
-                                    ["shift","z","x","c","v","b","n","m",",",".","/","shift"],
-                                    ["shift"]]
+        addKey(position: position, keyboard: keyboard, keyboardNode: keyboardNode)
         
+    }
+    
+    func addKey(position: SCNVector3, keyboard: SCNBox, keyboardNode: SCNNode) {
+        
+        var keyboardKeyList: [[String]] = [[]]
+        
+        if pressedShift {
+            keyboardKeyList = keyboardListPressedShift
+        } else {
+            keyboardKeyList = keyboardListNotPressedShift
+        }
+    
         var y = position.y - Float(keyboard.height)/2
         
-        for keyboarList in keyboardListNotShift {
+        for keyboarList in keyboardKeyList {
             
             y += Float(keyboard.height/7)
             var x = position.x - Float(keyboard.width)/2
@@ -60,7 +85,6 @@ class ARKeyBoard: SCNNode {
             }
       
         }
-        
         
     }
     
